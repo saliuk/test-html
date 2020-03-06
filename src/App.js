69097d6cde30ac92fd8a80e725a126255eaf0daa
saliuk <admin@InveritaUser15.localdomain>
2020-03-06 14:32:33 +0200

    2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>People</code>.
+          Hello <code>People2</code>.
         </p>
         <a
           className="App-link"
